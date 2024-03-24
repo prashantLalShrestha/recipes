df9240308b826a271ff4f43c1567a7891ee4c8cb
Prashant Shrestha <prashant.shrestha@elements.nl>
2024-03-24 13:30:23 +0100

    feat: inject search recipe to viewmodel

--- app/src/main/java/np/prashant/dev/recipes/ui/search/RecipeSearchViewModel.kt
+++ app/src/main/java/np/prashant/dev/recipes/ui/search/RecipeSearchViewModel.kt
@@ -1,9 +1,29 @@
 package np.prashant.dev.recipes.ui.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import np.prashant.dev.recipes.services.domain.recipes.usecases.SearchRecipe
 import javax.inject.Inject
 
 
 @HiltViewModel
-class RecipeSearchViewModel @Inject constructor() : ViewModel()
+class RecipeSearchViewModel @Inject constructor(
+    private val searchRecipe: SearchRecipe
+) : ViewModel() {
+
+
+    private var searchRecipeJob: Job? = null
+
+    init {
+        performSearchRecipe("pancakes")
+    }
+
+    fun performSearchRecipe(query: String) {
+        searchRecipeJob?.cancel()
+
+        searchRecipeJob = searchRecipe(query).launchIn(viewModelScope)
+    }
+}
